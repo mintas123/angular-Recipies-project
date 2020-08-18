ea1cfe41571f0f765953d19ef44649d2b02c9177
Jakub Mróz <kumroz@gmail.com>
2020-08-18 13:38:26 +0200

    ingredients in the recipe details

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import {Ingredient} from '../shared/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imageUrl: string;
+  public ingredients: Ingredient[];
 
 
-  constructor(name: string, desc: string, url: string) {
+  constructor(name: string, desc: string, url: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imageUrl = url;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,29 @@
 import {Recipe} from './recipe.model';
 import { EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Some recipe', 'this is a test recipe. It is a part with a description, some additional information about your recipe.', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
-    new Recipe('Other recipe', 'Need some more tedsting, but overall seems cool.', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg')
+    new Recipe('Tasty spaghet',
+      'Makes your knees weak and arms sweaty.',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg',
+      [
+        new Ingredient('Meat', 2),
+        new Ingredient('Pasta', 1),
+        new Ingredient('Tomato Sauce', 1),
+      ]
+      ),
+    new Recipe('Awesome schnitzel',
+      'You will crave it for days.',
+      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries', 40),
+        new Ingredient('Lemon', 1)
+      ])
   ];
 
   getRecipes() {
